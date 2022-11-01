d67285c1d72208c4ddb8f509a69de4eb0542f6ab
seyha <sinseyha1x@gmail.com>
2022-11-01 13:59:06 +0700

    xom

--- Client.java
+++ Client.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
 
 /**
  * @author seyha.sin Nov 1, 2022/2022 : 9:45:36 AM
@@ -5,12 +9,33 @@
 public class Client {
 
 	public static void main(String[] args) {
-		if (args.length != 0) {
+
+		Socket socket = null;
+		if (args.length < 2) {
 
 			System.out.println("client" + args.length);
+			System.exit(0);
+		}
+		try {
+			socket = new Socket("localhost", 9999);
+			// Create input and output streams to read from and write to the server
+			PrintStream out = new PrintStream(socket.getOutputStream());
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out.println(args[0]);
+			out.println(args[1]);
+			String line = in.readLine();
+			while (line != null) {
+				System.out.println(line);
+				line = in.readLine();
+			}
 
-		} else {
-			System.out.println("Incorrect command!");
+			// Close our streams
+			in.close();
+			out.close();
+			socket.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
 }
--- Server.java
+++ Server.java
@@ -1,6 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -21,14 +23,20 @@ public class Server {
 		System.out.println("Server running");
 		try {
 			serverSocket = new ServerSocket(9999);
+			Socket socket = serverSocket.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			System.out.println("The server is waiting for client...." + serverSocket.getInetAddress());
 			while (true) {
-				System.out.println("The server is waiting for client....");
-				Socket socket = serverSocket.accept();
-				if ("Fibo".equalsIgnoreCase(command)) {
-					pool = Executors.newFixedThreadPool(fibonacciThreadPool);
-					FibonacciThreadWorker fibonaciThreadWorker = new FibonacciThreadWorker(socket, index, number);
-					pool.execute(fibonaciThreadWorker);
-				}
+                 System.out.print( " client ==  > " + number);
+             	pool = Executors.newFixedThreadPool(fibonacciThreadPool);
+				FibonacciThreadWorker fibonaciThreadWorker = new FibonacciThreadWorker(socket, index, number);
+				pool.execute(fibonaciThreadWorker);
+				System.out.println();
+//				if ("Fibo".equalsIgnoreCase(command)) {
+//					pool = Executors.newFixedThreadPool(fibonacciThreadPool);
+//					FibonacciThreadWorker fibonaciThreadWorker = new FibonacciThreadWorker(socket, index, number);
+//					pool.execute(fibonaciThreadWorker);
+//				}
 				if ("Fact".equalsIgnoreCase(command)) {
 					pool = Executors.newFixedThreadPool(factorialThreadPool);
 					FactorialThreadWorker factorialThreadWorker = new FactorialThreadWorker(socket, index, number);
